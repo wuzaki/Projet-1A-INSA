fcd5d719f3875db4ee2e72c48d489c0d6166b804
Wuzaki <wuzaki.one@gmail.com>
2026-06-04 16:38:34 +0200

    update

--- maps.py
+++ maps.py
@@ -33,8 +33,8 @@ class WorldGraph:
         self.tile_maps = dict() # Matrice de 0 et 1 pour l'algorithme A*
 
         # self.load_map("map_test", zoom=2)
-        self.load_map("intro_beta", zoom=1.6)
-        self.load_map("beta", zoom=1.6)
+        self.load_map("intro_beta", zoom=2)
+        self.load_map("beta", zoom=2)
         self.spawn_player("player")
 
     def spawn_player(self, name):
